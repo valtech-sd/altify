e36b6d3f8e1c90ad027dbd1c584006f8dc9c4478
Matias Arabolaza <matias2205@gmail.com>
2023-01-18 13:46:02 -0300

    Azure file with environment variables and openAPI changes

--- helpers/azure.js
+++ helpers/azure.js
@@ -8,95 +8,97 @@ import { ApiKeyCredentials } from "@azure/ms-rest-js";
 import RandomImageUrl from "./DefaultImages";
 
 // Authentication requirements
-const key = "test";
+const key = process.env.NEXT_PUBLIC_MS_CV_API_KEY;
 
-const endpoint = "test";
-
-console.log(`key = ${key}`);
-console.log(`endpoint = ${endpoint}`);
+const endpoint = process.env.NEXT_PUBLIC_MS_CV_ENDPOINT;
 
 // Cognitive service features
 const visualFeatures = [
-  "ImageType",
-  "Faces",
-  "Adult",
-  "Categories",
-  "Color",
-  "Tags",
-  "Description",
-  "Objects",
-  "Brands",
+    "ImageType",
+    "Faces",
+    "Adult",
+    "Categories",
+    "Color",
+    "Tags",
+    "Description",
+    "Objects",
+    "Brands",
 ];
 
 export const isConfigured = () => {
-  const result =
-    key && endpoint && key.length > 0 && endpoint.length > 0 ? true : false;
-  console.log(`key = ${key}`);
-  console.log(`endpoint = ${endpoint}`);
-  console.log(`ComputerVision isConfigured = ${result}`);
-  return result;
+    const result =
+        key && endpoint && key.length > 0 && endpoint.length > 0 ? true : false;
+    console.log(`key = ${key}`);
+    console.log(`endpoint = ${endpoint}`);
+    console.log(`ComputerVision isConfigured = ${result}`);
+    return result;
 };
 
 // Computer Vision detected Printed Text
 const includesText = async (tags) => {
-  return tags.filter((el) => {
-    return el.name.toLowerCase() === "text";
-  });
+    return tags.filter((el) => {
+        return el.name.toLowerCase() === "text";
+    });
 };
 // Computer Vision detected Handwriting
 const includesHandwriting = async (tags) => {
-  return tags.filter((el) => {
-    return el.name.toLowerCase() === "handwriting";
-  });
+    return tags.filter((el) => {
+        return el.name.toLowerCase() === "handwriting";
+    });
 };
 // Wait for text detection to succeed
 const wait = (timeout) => {
-  return new Promise((resolve) => {
-    setTimeout(resolve, timeout);
-  });
+    return new Promise((resolve) => {
+        setTimeout(resolve, timeout);
+    });
 };
 
 // Analyze Image from URL
 export const computerVision = async (url) => {
-  // authenticate to Azure service
-  const computerVisionClient = new ComputerVisionClient(
-    new ApiKeyCredentials({ inHeader: { "Ocp-Apim-Subscription-Key": key } }),
-    endpoint
-  );
+    // authenticate to Azure service
+    const computerVisionClient = new ComputerVisionClient(
+        new ApiKeyCredentials({
+            inHeader: { "Ocp-Apim-Subscription-Key": key },
+        }),
+        endpoint
+    );
 
-  // get image URL - entered in form or random from Default Images
-  const urlToAnalyze = url || RandomImageUrl();
+    // get image URL - entered in form or random from Default Images
+    const urlToAnalyze = url || RandomImageUrl();
 
-  // analyze image
-  const analysis = await computerVisionClient.analyzeImage(urlToAnalyze, {
-    visualFeatures,
-  });
+    // analyze image
+    const analysis = await computerVisionClient.analyzeImage(urlToAnalyze, {
+        visualFeatures,
+    });
 
-  // text detected - what does it say and where is it
-  if (includesText(analysis.tags) || includesHandwriting(analysis.tags)) {
-    analysis.text = await readTextFromURL(computerVisionClient, urlToAnalyze);
-  }
+    // text detected - what does it say and where is it
+    if (includesText(analysis.tags) || includesHandwriting(analysis.tags)) {
+        analysis.text = await readTextFromURL(
+            computerVisionClient,
+            urlToAnalyze
+        );
+    }
 
-  // all information about image
-  return { URL: urlToAnalyze, ...analysis };
+    // all information about image
+    return { URL: urlToAnalyze, ...analysis };
 };
 // analyze text in image
 const readTextFromURL = async (client, url) => {
-  let result = await client.read(url);
-  let operationID = result.operationLocation.split("/").slice(-1)[0];
+    let result = await client.read(url);
+    let operationID = result.operationLocation.split("/").slice(-1)[0];
 
-  // Wait for read recognition to complete
-  // result.status is initially undefined, since it's the result of read
-  const start = Date.now();
-  console.log(`${start} -${result?.status} `);
+    // Wait for read recognition to complete
+    // result.status is initially undefined, since it's the result of read
+    const start = Date.now();
+    console.log(`${start} -${result?.status} `);
 
-  while (result.status !== "succeeded") {
-    await wait(500);
-    console.log(`${Date.now() - start} -${result?.status} `);
-    result = await client.getReadResult(operationID);
-  }
+    while (result.status !== "succeeded") {
+        await wait(500);
+        console.log(`${Date.now() - start} -${result?.status} `);
+        result = await client.getReadResult(operationID);
+    }
 
-  // Return the first page of result.
-  // Replace[0] with the desired page if this is a multi-page file such as .pdf or.tiff.
-  return result.analyzeResult;
+    // Return the first page of result.
+    // Replace[0] with the desired page if this is a multi-page file such as .pdf or.tiff.
+    return result.analyzeResult;
 };
