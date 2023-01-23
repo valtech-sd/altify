b0259af35e05a60fe3b7f1f990c873f627231f51
Matias Arabolaza <matias.arabolaza@valtech.com>
2023-01-23 16:47:47 -0300

    Update .env example

--- constants/constants.js
+++ constants/constants.js
@@ -1,2 +1,4 @@
-export const serverUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3030';
-export const localUrl = process.env.NEXT_PUBLIC_LOCAL_URL || 'http://localhost:3000';
+export const serverUrl =
+    process.env.NEXT_PUBLIC_API_URL || "http://localhost:3030";
+export const localUrl =
+    process.env.NEXT_PUBLIC_LOCAL_URL || "http://localhost:3030";
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,83 @@
-import Head from 'next/head';
-import { useState } from 'react';
-import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
+import Head from "next/head";
+import { useState } from "react";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 
-import styles from './index.module.css';
-import { serverUrl } from '../constants/constants';
+import styles from "./index.module.css";
+import { serverUrl } from "../constants/constants";
 import Image from "../components/image";
 
 export default function Home() {
-  const [imageInput, setImageInput] = useState('');
-  const [result, setResult] = useState();
-  const [loading, setLoading] = useState(false);
+    const [imageInput, setImageInput] = useState("");
+    const [result, setResult] = useState();
+    const [loading, setLoading] = useState(false);
 
-  async function onSubmit(event) {
-    event.preventDefault();
-    setLoading(true);
-    const response = await fetch(`${serverUrl}/generate`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ url: imageInput }),
-    });
-    const data = await response.json();
-    setResult(data.result);
-    setLoading(false);
-  }
+    async function onSubmit(event) {
+        event.preventDefault();
+        setLoading(true);
+        const response = await fetch(`${serverUrl}/generate`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ url: imageInput }),
+        });
+        const data = await response.json();
+        setResult(data.result);
+        setLoading(false);
+    }
 
-  return (
-    <div>
-      <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/valtechLogo-black.png" />
-      </Head>
+    return (
+        <div>
+            <Head>
+                <title>OpenAI Quickstart</title>
+                <link rel="icon" href="/valtechLogo-black.png" />
+            </Head>
 
-      <main className={styles.main}>
-        <form onSubmit={onSubmit}>
-          <img src="./valtechLogo-black.png" className={styles.icon} />
-          <input type="text" name="image url" placeholder="Enter an url" value={imageInput} onChange={(e) => setImageInput(e.target.value)} />
-          <input type="submit" value="Analize URL" />
-        </form>
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            width: '80%',
-            marginTop: '20px',
-          }}
-        >
-          <div style={{ margin: '0 auto' }}>{loading && <div className="loading-bar"></div>}</div>
-          {result && (
-            <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
-              <Masonry gutter="1.5rem">
-                {Array.isArray(result) &&
-                  result.map((image, idx) => {
-                    return <Image key={`altify-image-${idx}`} src={image.image} />;
-                  })}
-              </Masonry>
-            </ResponsiveMasonry>
-          )}
+            <main className={styles.main}>
+                <form onSubmit={onSubmit}>
+                    <img
+                        src="./valtechLogo-black.png"
+                        className={styles.icon}
+                    />
+                    <input
+                        type="text"
+                        name="image url"
+                        placeholder="Enter an url"
+                        value={imageInput}
+                        onChange={(e) => setImageInput(e.target.value)}
+                    />
+                    <input type="submit" value="Analize URL" />
+                </form>
+                <div
+                    style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        width: "80%",
+                        marginTop: "20px",
+                    }}
+                >
+                    <div style={{ margin: "0 auto" }}>
+                        {loading && <div className="loading-bar"></div>}
+                    </div>
+                    {result && (
+                        <ResponsiveMasonry
+                            columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}
+                        >
+                            <Masonry gutter="1.5rem">
+                                {Array.isArray(result) &&
+                                    result.map((image, idx) => {
+                                        return (
+                                            <Image
+                                                key={`altify-image-${idx}`}
+                                                src={image.image}
+                                            />
+                                        );
+                                    })}
+                            </Masonry>
+                        </ResponsiveMasonry>
+                    )}
+                </div>
+            </main>
         </div>
-      </main>
-    </div>
-  );
+    );
 }
