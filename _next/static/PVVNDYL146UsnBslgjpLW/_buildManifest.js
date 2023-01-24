abf1145ad4a763a5ac4a72e455787b554e94fa26
cvols-valtech <102599362+cvols-valtech@users.noreply.github.com>
2023-01-24 17:00:43 +0000

    Deploying to gh-pages from @ valtech-sd/altify@bd83769e19972147c0f0b905854142dbbac191e8 🚀

---
+++ _next/static/PVVNDYL146UsnBslgjpLW/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/828-f1e7d81c50ed76de.js","static/css/110049f5a1bfe5c6.css","static/chunks/pages/index-5f6d907646ae30b4.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
