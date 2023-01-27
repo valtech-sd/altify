f2c6fcf00bb158fe92427f9d2dffbcfbca066002
cvols-valtech <chris.volonnino@valtech.com>
2023-01-27 13:53:59 -0500

    feat: missed features from previous merge conflict

--- components/ImageCard/ImageCard.jsx
+++ components/ImageCard/ImageCard.jsx
@@ -1,55 +1,39 @@
-import { object } from "prop-types";
-import { useState } from "react";
-import { AltText, Azure, CloudVision } from "../../components";
-import {
-    SelectDropdown,
-    DropdownLabel,
-    SelectDropdownContainer,
-} from "./styles";
+import { object } from 'prop-types';
+import { useState } from 'react';
+import { AltText, Azure, CloudVision } from '../../components';
+import { SelectDropdown, DropdownLabel, SelectDropdownContainer } from './styles';
 
 const propTypes = {
-    results: object,
+  results: object,
 };
 
-const ImageCard = ({ image, password }) => {
-    const [creativity, setCreativity] = useState(0);
+const ImageCard = ({ image, password, index }) => {
+  const [creativity, setCreativity] = useState(0);
 
-    return (
-        <div style={{ marginTop: 5 }}>
-            <div
-                style={{
-                    display: "flex",
-                    flexDirection: "column",
-                }}
-            >
-                <img src={image.image} alt="" style={{ width: "100%" }} />
-                <AltText altText={image.current} />
-                <DropdownLabel>Creativity:</DropdownLabel>
-                <SelectDropdownContainer>
-                    <SelectDropdown
-                        name="creativity"
-                        id="creativity"
-                        onChange={(e) => setCreativity(e.target.value)}
-                    >
-                        <option value="0">Light</option>
-                        <option value="0.5">Medium</option>
-                        <option value="1">High</option>
-                    </SelectDropdown>
-                </SelectDropdownContainer>
+  return (
+    <div style={{ marginTop: 5 }}>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <img src={image.image} alt="" style={{ width: '100%' }} />
+        <AltText altText={image.current} index={index + 1} />
+        <DropdownLabel>Creativity:</DropdownLabel>
+        <SelectDropdownContainer>
+          <SelectDropdown name="creativity" id="creativity" onChange={(e) => setCreativity(e.target.value)}>
+            <option value="0">Light</option>
+            <option value="0.5">Medium</option>
+            <option value="1">High</option>
+          </SelectDropdown>
+        </SelectDropdownContainer>
 
-                <Azure
-                    src={image}
-                    password={password}
-                    creativity={creativity}
-                />
-                <CloudVision
-                    src={image}
-                    password={password}
-                    creativity={creativity}
-                />
-            </div>
-        </div>
-    );
+        <Azure src={image} password={password} creativity={creativity} />
+        <CloudVision src={image} password={password} creativity={creativity} />
+      </div>
+    </div>
+  );
 };
 
 ImageCard.propTypes = propTypes;
--- pages/index.js
+++ pages/index.js
@@ -1,134 +1,103 @@
-import Head from "next/head";
-import { useEffect, useState } from "react";
-import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+import Head from 'next/head';
+import { useState } from 'react';
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry';
 
-import styles from "./index.module.css";
-import { serverUrl } from "../constants/constants";
-import { Input, ImageCard } from "../components";
+import styles from './index.module.css';
+import { serverUrl } from '../constants/constants';
+import { Input, ImageCard } from '../components';
 
 export default function Home() {
-    const [imageInput, setImageInput] = useState("");
-    const [password, setPassword] = useState("");
-    const [result, setResult] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [correctPassword, setCorrectPassword] = useState(false);
-    const [length, setLength] = useState(0);
-    let counter = 0;
+  const [imageInput, setImageInput] = useState('');
+  const [password, setPassword] = useState('');
+  const [result, setResult] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [correctPassword, setCorrectPassword] = useState(false);
 
-    function clearState() {
-        setResult(null);
+  function clearState() {
+    setResult(null);
+  }
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    if (length > 0) {
+      clearState();
     }
-
-    async function onSubmit(event) {
-        event.preventDefault();
-        if (length > 0) {
-            clearState();
-        }
-        setLoading(true);
-        const response = await fetch(`${serverUrl}/generate`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                authentication: password,
-            },
-            body: JSON.stringify({ url: imageInput }),
-        });
-        if (response.status === 401) {
-            setLoading(false);
-            alert("Wrong password");
-            return;
-        }
-        setCorrectPassword(true);
-        const data = await response.json();
-        if (data.result.length === 0) {
-            setLoading(false);
-            alert("No images found");
-            return;
-        }
-        // setLength(data.result.length);
-        setResult(data.result);
-        setLoading(false);
+    setLoading(true);
+    const response = await fetch(`${serverUrl}/generate`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        authentication: password,
+      },
+      body: JSON.stringify({ url: imageInput }),
+    });
+    if (response.status === 401) {
+      setLoading(false);
+      alert('Wrong password');
+      return;
     }
-
-    function handlePasswordChange(event) {
-        setPassword(event.target.value);
+    setCorrectPassword(true);
+    const data = await response.json();
+    if (data.result.length === 0) {
+      setLoading(false);
+      alert('No images found');
+      return;
     }
+    setResult(data.result.filter((res) => !res.src?.endsWith('.svg')));
+    setLoading(false);
+  }
 
-    function handleInputChange(event) {
-        setImageInput(event.target.value);
-    }
+  function handlePasswordChange(event) {
+    setPassword(event.target.value);
+  }
 
-    useEffect(() => {
-        const svg = result?.filter((res) => res.src?.endsWith(".svg"));
-        setLength(result?.length - svg?.length || 0);
-    }, [result]);
+  function handleInputChange(event) {
+    setImageInput(event.target.value);
+  }
 
-    return (
-        <div>
-            <Head>
-                <title>OpenAI Quickstart</title>
-                <link rel="icon" href="/valtechLogo-black.png" />
-            </Head>
+  return (
+    <div>
+      <Head>
+        <title>OpenAI Quickstart</title>
+        <link rel="icon" href="/valtechLogo-black.png" />
+      </Head>
 
-            <main className={styles.main}>
-                <form onSubmit={onSubmit}>
-                    <img
-                        src="./valtechLogo-black.png"
-                        className={styles.icon}
-                    />
-                    <Input
-                        type="text"
-                        name="image url"
-                        placeholder="Enter a url"
-                        value={imageInput}
-                        onChange={(e) => setImageInput(e.target.value)}
-                    />
-                    {!correctPassword && (
-                        <Input
-                            type="password"
-                            name="password"
-                            placeholder="Enter the password"
-                            value={password}
-                            onChange={handlePasswordChange}
-                        />
-                    )}
-                    <input
-                        disabled={!password || !imageInput}
-                        type="submit"
-                        value="Analize URL"
-                    />
-                </form>
-                <div
-                    style={{
-                        display: "flex",
-                        flexDirection: "column",
-                        width: "80%",
-                        marginTop: "20px",
-                    }}
-                >
-                    <div style={{ margin: "0 auto" }}>
-                        {loading && <div className="loading-bar"></div>}
-                    </div>
-                    {result && (
-                        <ResponsiveMasonry
-                            columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}
-                        >
-                            <Masonry gutter="1.5rem">
-                                {Array.isArray(result) &&
-                                    result.map((image, idx) => {
-                                        return (
-                                            <ImageCard
-                                                key={`altify-image-${idx}`}
-                                                image={image}
-                                                password={password}
-                                            />
-                                        );
-                                    })}
-                            </Masonry>
-                        </ResponsiveMasonry>
-                    )}
-                </div>
-            </main>
+      <main className={styles.main}>
+        <form onSubmit={onSubmit}>
+          <img src="./valtechLogo-black.png" className={styles.icon} />
+          <Input type="text" name="image url" placeholder="Enter a url" value={imageInput} onChange={handleInputChange} />
+          <Input
+            disabled={correctPassword}
+            type="password"
+            name="password"
+            placeholder="Enter the password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+          <input disabled={!password || !imageInput} type="submit" value="Analize URL" />
+          <p>Images found: {result?.length}</p>
+        </form>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            width: '80%',
+            marginTop: '20px',
+          }}
+        >
+          <div style={{ margin: '0 auto' }}>{loading && <div className="loading-bar"></div>}</div>
+          {result && (
+            <ResponsiveMasonry columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}>
+              <Masonry gutter="1.5rem">
+                {Array.isArray(result) &&
+                  result.map((image, idx) => {
+                    return <ImageCard key={`altify-image-${idx}`} index={idx} image={image} password={password} />;
+                  })}
+              </Masonry>
+            </ResponsiveMasonry>
+          )}
         </div>
-    );
+      </main>
+    </div>
+  );
 }
