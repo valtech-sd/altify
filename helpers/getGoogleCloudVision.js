69761aa68204cb5855550351f8242bcdcfd51ac1
cvols-valtech <chris.volonnino@valtech.com>
2023-01-27 14:30:54 -0500

    chore: remove localURL

--- helpers/getGoogleCloudVision.js
+++ helpers/getGoogleCloudVision.js
@@ -4,7 +4,6 @@ export default async function getGoogleCloudVision(src, password) {
   const headers = {
     Accept: 'application/json',
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': `${localUrl}`,
     'Access-Control-Allow-Methods': 'POST',
     'Access-Control-Allow-Headers': 'Content-Type, Authorization',
     image: src,
