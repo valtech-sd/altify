9bfbada0d4a96c9f547c3a04a3cf3d4ea5ce310f
cvols-valtech <chris.volonnino@valtech.com>
2023-02-01 16:21:11 -0500

    feat: add password protection

---
+++ components/Alert/Alert.jsx
@@ -0,0 +1,79 @@
+import { forwardRef, useState } from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+import { func, string } from 'prop-types';
+import { styled } from '@mui/system';
+
+import { serverUrl } from '../../constants/constants';
+import Input from '../Input';
+
+const green = 'rgb(118, 248, 176)';
+
+const Butt = styled(Button)({
+  color: green,
+  fontWeight: 'bold',
+  letterSpacing: '1px',
+})
+
+const Transition = forwardRef(function Transition(props, ref) {
+  return <Slide direction="down" ref={ref} {...props} />;
+});
+
+const propTypes = {
+  setCorrectPassword: func.isRequired,
+  handlePasswordChange: func.isRequired,
+  password: string.isRequired,
+};
+
+const Alert = ({ setCorrectPassword, handlePasswordChange, password }) => {
+  const [open, setOpen] = useState(true);
+
+  async function handleClose() {
+    if (!password) return;
+
+    try {
+      const response = await fetch(`${serverUrl}/verifyPassword`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          authentication: password,
+        },
+      });
+
+      if (response.status === 401) {
+        alert('Incorrect password');
+        return;
+      }
+
+      if (response.status === 200) {
+        setCorrectPassword(true);
+        setOpen(false);
+        return;
+      }
+    } catch (error) {
+      console.log('error: ', error);
+    }
+  }
+
+  return (
+    <div>
+      <Dialog open={open} TransitionComponent={Transition} keepMounted aria-describedby="alert-dialog-slide-description">
+        <DialogTitle>Please enter the password</DialogTitle>
+        <DialogContent>
+          <Input type="password" fullWidth value={password} onChange={handlePasswordChange} />
+        </DialogContent>
+        <DialogActions>
+          <Butt onClick={handleClose}>Enter</Butt>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+};
+
+Alert.propTypes = propTypes;
+
+export default Alert;
--- constants/constants.js
+++ constants/constants.js
@@ -1,4 +1,1 @@
-export const serverUrl =
-    process.env.NEXT_PUBLIC_API_URL || "http://localhost:3030";
-export const localUrl =
-    process.env.NEXT_PUBLIC_LOCAL_URL || "http://localhost:3030";
+export const serverUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3030';
