3ee2ec38197b244ed2761e22f0526aba54936867
max-mcgee-valtech <100313226+max-mcgee-valtech@users.noreply.github.com>
2023-02-04 00:27:37 +0000

    Deploying to gh-pages from @ valtech-sd/altify@78c82ef59f6ca369e8851b2ecf8fffc2596d6d1f 🚀

--- _next/static/KrCR-y11xbZnh-dEryILx/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/514-ae99ac3fdbe8571b.js","static/css/b72a88361faef74e.css","static/chunks/pages/index-a47c7e08292f03c6.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
