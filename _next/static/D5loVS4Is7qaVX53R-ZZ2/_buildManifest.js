1bc77771baa7415a268807bc9bdf895e6e244eeb
max-mcgee-valtech <100313226+max-mcgee-valtech@users.noreply.github.com>
2023-02-07 00:40:33 +0000

    Deploying to gh-pages from @ valtech-sd/altify@16a16834c48b29a2cc39e8361b5da1d3f5cb0a2d 🚀

---
+++ _next/static/D5loVS4Is7qaVX53R-ZZ2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/288-c6b7513c0cf0bf98.js","static/css/b72a88361faef74e.css","static/chunks/pages/index-c5992946c6cc04be.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/wu3FlMwiwO6fr53P6-bBO/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/288-c6b7513c0cf0bf98.js","static/css/b72a88361faef74e.css","static/chunks/pages/index-20a6ebb6cd91e14c.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
